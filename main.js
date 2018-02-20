5757cbc8a47794c85c6eba8d09170fe13545040d
Alejandro Gaspar <caxpper@hotmail.com>
2018-02-19 17:11:18 -0800

    calculator version1 - Alejandro Gaspar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(initApp);
-var extended = false;
+var extra_flag = false;
+var history_flag = false;
 
 function callback(type,value) {
     var displayText = $('.display').text();
@@ -8,7 +9,7 @@ function callback(type,value) {
         case "delete":
             $('.display').text("");
             break;
-        case "decimal":
+        case "special":
             $('.display').text(value);
             break;
         case "result":
@@ -26,12 +27,14 @@ function callback(type,value) {
 var my_calculator = new Calculator(callback);
 
 function initApp(){
+    my_calculator.setJqueryHistoryElement($('.history'));
     attachedHandlers();
 }
 
 function attachedHandlers() {
     $('button').on('click',buttonHandler);
-    $('.extend').on('click',extendPanel);
+    $('.extra_extend').on('click',extendPanel);
+    $('.history_extend').on('click',extendHistory);
 }
 
 function buttonHandler() {
@@ -45,8 +48,7 @@ function buttonHandler() {
             my_calculator.deleteItems();
             break;
         case '=':
-            var history = [];
-            result = my_calculator.calculateTotal(1,history);
+            result = my_calculator.calculateTotal(1);
             break;
         default:
             my_calculator.addItem(val);
@@ -55,16 +57,66 @@ function buttonHandler() {
 }
 
 function extendPanel() {
-
-    if(!extended) {
-        extended = true;
+    if(extra_flag) {
+        extra_flag = false;
+        $('.operators').removeClass("operators_extend");
+        $('.left_side').removeClass("left_side_extend");
+        $('.calculator').removeClass("calculator_extend");
+        $('.calculator').removeClass("calculator_history_extend");
+        $('.main').removeClass("main_history_extend");
+        if(history_flag){
+            $('.calculator').addClass("calculator_history");
+            $('.main').addClass("main_history");
+        }
+        $('.extraButtons').addClass("hidden");
+        $('.extra_extend').text('►');
+        $('.display').removeClass('display_extend');
+    }else {
+        extra_flag = true;
         $('.operators').addClass("operators_extend");
         $('.left_side').addClass("left_side_extend");
         $('.calculator').addClass("calculator_extend");
         $('.extraButtons').removeClass("hidden");
-    }else{
-        $('.calculator').addClass("calculator_history");
-        $('.main').addClass("main_history");
-        $('.history').removeClass("hidden");
+        $('.extra_extend').text('◄');
+        if (history_flag) {
+            $('.display').addClass('display_extend');
+            $('.calculator').addClass("calculator_history_extend");
+            $('.calculator').removeClass("calculator_extend");
+            $('.main').addClass("main_history_extend");
+        }
     }
 }
+
+
+function extendHistory(){
+    if(!history_flag){
+        history_flag = true;
+        $('.calculator').addClass("calculator_history");
+        $('.calculator').removeClass("calculator_extend");
+        $('.main').addClass("main_history");
+        $('.history').removeClass("hidden");
+        $('.history_extend').text('►');
+        if(extra_flag) {
+            $('.display').addClass('display_extend');
+            $('.main').removeClass("main_history");
+            $('.main').addClass("main_history_extend");
+            $('.calculator').removeClass("calculator_history");
+            $('.calculator').addClass("calculator_history_extend");
+        }
+    }else{
+        history_flag = false;
+        $('.calculator').removeClass("calculator_history_extend");
+        $('.calculator').removeClass("calculator_history");
+        $('.main').removeClass("main_history");
+        $('.main').removeClass("main_history_extend");
+        if(extra_flag){
+            $('.calculator').addClass("calculator_extend");
+        }
+        $('.history').addClass("hidden");
+        $('.history_extend').text('◄ History');
+        $('.display').removeClass('display_extend');
+
+    }
+
+}
+
